ecf13247d85cdf055251d2c21675e357db6b9439
Krivoox <krivoox@gmail.com>
2023-10-30 08:39:01 -0300

    install tailwind in proyect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,41 @@
-import { signIn, signOut, useSession } from 'next-auth/react'
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 export default function Home() {
-  const { data: session, } = useSession()
+  const { data: session } = useSession();
   if (session) {
     return (
-      <div style={{ backgroundColor: '#2C2F33', color: '#FFFFFF', padding: '20px', borderRadius: '10px' }}>
+      <div className="bg-[#2C2F33] text-white p-5 rounded-lg">
         <p>Signed in as {session.user?.email}</p>
-        <button 
-          onClick={() => signOut()} 
-          style={{ backgroundColor: '#7289DA', color: '#FFFFFF', border: 'none', borderRadius: '5px', padding: '10px' }}
+        <button
+          onClick={() => signOut()}
+          style={{
+            backgroundColor: '#7289DA',
+            color: '#FFFFFF',
+            border: 'none',
+            borderRadius: '5px',
+            padding: '10px',
+          }}
         >
           Sign out
         </button>
       </div>
-    )
+    );
   }
   return (
-    <div style={{ backgroundColor: '#2C2F33', color: '#FFFFFF', padding: '20px', borderRadius: '10px' }}>
+    <div className="bg-[#2C2F33] p-10 m-5 rounded-md">
       <p>Not signed in</p>
-      <button 
-        onClick={() => signIn("discord")} 
-        style={{ backgroundColor: '#7289DA', color: '#FFFFFF', border: 'none', borderRadius: '5px', padding: '10px' }}
+      <button
+        onClick={() => signIn('discord')}
+        style={{
+          backgroundColor: '#7289DA',
+          color: '#FFFFFF',
+          border: 'none',
+          borderRadius: '5px',
+          padding: '10px',
+        }}
       >
         Sign in
       </button>
     </div>
-  )
+  );
 }
